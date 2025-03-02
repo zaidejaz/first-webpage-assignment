9ffe0815cc863c63a44d239feca1088fb7274854
zaidejaz <zaidejaz521@gmail.com>
2025-03-02 19:15:49 +0500

    Update grades table and remove avatar component

--- app/components/GradesTable.tsx
+++ app/components/GradesTable.tsx
@@ -15,30 +15,18 @@ type Subject = {
 }
 
 const subjects = [
-    { name: "Applied Physics", creditHours: 3, grade: "A" },
-    { name: "English Composition & Comprehension", creditHours: 3, grade: "B+" },
     { name: "Programming Fundamentals", creditHours: 4, grade: "B+" },
     { name: "Object Oriented Programming", creditHours: 4, grade: "B+" },
-    { name: "Communication and Presentation Skills", creditHours: 3, grade: "B+" },
-    { name: "Quranic Studies", creditHours: 3, grade: "B+" },
-    { name: "Introduction to Social Media Marketing", creditHours: 3, grade: "B+" },
     { name: "Introduction to Computer Programming", creditHours: 3, grade: "B" },
     { name: "Discrete Structures", creditHours: 4, grade: "C" },
-    { name: "Calculus & Analytical Geometry", creditHours: 3, grade: "C" },
     { name: "Digital Logic Design", creditHours: 3, grade: "C" },
-    { name: "Multivariate Calculus", creditHours: 3, grade: "C" },
-    { name: "Linear Algebra", creditHours: 3, grade: "A" },
     { name: "Introduction to Software Engineering", creditHours: 3, grade: "D" },
-    { name: "Islamic World View and Civilization", creditHours: 3, grade: "B+" },
     { name: "Introduction to Database System", creditHours: 4, grade: "B+" },
     { name: "Data Structure and Algorithms", creditHours: 4, grade: "B+" },
     { name: "Object Oriented Analysis and Design", creditHours: 3, grade: "B" },
-    { name: "Differential Equation", creditHours: 3, grade: "B+" },
-    { name: "Probability & Statistics", creditHours: 3, grade: "B+" },
     { name: "Design and Analysis of Algorithms", creditHours: 3, grade: "D+" },
     { name: "Operating System", creditHours: 4, grade: "C" },
     { name: "Computer Networks", creditHours: 4, grade: "A" },
-    { name: "Numerical Computing", creditHours: 3, grade: "C" },
     { name: "Advance Computer Programming", creditHours: 4, grade: "B+" },
 ];
 
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
 
 import { GradesTable } from "~/components/GradesTable";
-import { ZaidAvatar } from "~/components/ZaidAvatar";
 
 export const meta: MetaFunction = () => {
   return [
@@ -14,16 +13,13 @@ export default function Index() {
   return (
     <div className="flex h-screen items-center justify-center">
       <div className="flex flex-col items-center gap-8">
-        <header className="flex items-center gap-2">
-          <div className="flex flex-col items-center gap-2">
+        <header className="flex flex-col items-center gap-2">
             <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
               Zaid Ejaz
             </h1>
             <h3 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
               4574-FOC/BSCS/F22
             </h3>
-          </div>
-          <ZaidAvatar />
         </header>
         <div className="max-h-[500px] overflow-y-auto scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-tranparent">
           <GradesTable />
